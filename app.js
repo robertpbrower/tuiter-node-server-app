1084bf3c7fd19e79ca96551052e99dd321b474f1
Rob Brower <robertpbrower@gmail.com>
2023-04-06 11:16:07 -0400

    a9 mongodb

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ import UserController
   from "./controllers/users/users-controller.js"
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+  || 'mongodb://127.0.0.1:27017/tuiter'
+
 const app = express();
+mongoose.connect(CONNECTION_STRING);
 app.use(cors());
 app.use(express.json());
 TuitsController(app);
